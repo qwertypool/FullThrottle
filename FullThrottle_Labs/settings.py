eb3c3a630b1a3fa2ddc11e7f97a9a1f79856bc49
Deepa Pandey <deepapandey364@gmail.com>
2020-06-20 21:59:08 +0530

    Update settings.py

--- FullThrottle_Labs/settings.py
+++ FullThrottle_Labs/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,3 +130,4 @@ REST_FRAMEWORK = {
 
     'DATETIME_FORMAT': "%b %d %Y  %I:%M%p",
 }
+django_heroku.settings(locals())
